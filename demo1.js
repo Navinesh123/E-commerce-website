7fabed2bf7d49346c45cea04097fcd6c8422a611
Navinesh K <navineshnavinesh50@gmail.com>
2026-05-10 20:29:58 +0530

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,8 @@
+var submit=document.querySelector(".submit")
+var final=document.querySelector(".final")
+function message(){
+    alert("your order has been successfully registered");
+}
+function message1(){
+    alert("Thank for you Registration")
+}
